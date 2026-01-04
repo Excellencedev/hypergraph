5111dbfc0ce4f7c4d50695c4c8a3dda1edda2be0
seveibar <seveibar@gmail.com>
2026-01-04 15:44:03 -0800

    wip topology program

--- scripts/generate-jumper-grid-topology.ts
+++ scripts/generate-jumper-grid-topology.ts
@@ -1,3 +1,8 @@
+import type { JPort, JRegion } from "../lib/JumperGraphSolver/jumper-types"
+import { getSvgFromGraphicsObject } from "graphics-debug"
+import { JumperGraphSolver } from "../lib/JumperGraphSolver/JumperGraphSolver"
+import { visualizeJumperGraph } from "../lib/JumperGraphSolver/visualizeJumperGraph"
+
 const dims0603 = {
   padToPad: 1.65,
   padLength: 0.8,
@@ -14,3 +19,35 @@ const dims0603 = {
 // for a total of 16 regions/jumper
 // General regions have points that connect the regions on the edge
 // of each region they're adjacent to (but not diagonal from)
+
+export const generateSingleJumperRegions = ({
+  center,
+  idPrefix,
+}: {
+  center: { x: number; y: number }
+  idPrefix: string
+}) => {
+  const regions: JRegion[] = []
+  const ports: JPort[] = []
+
+  // TODO
+
+  return {
+    regions,
+    ports,
+  }
+}
+
+const singleJumperTopology = generateSingleJumperRegions({
+  center: { x: 0, y: 0 },
+  idPrefix: "singleJumper",
+})
+Bun.write(
+  "singlejumper.svg",
+  getSvgFromGraphicsObject(
+    visualizeJumperGraph({
+      ports: singleJumperTopology.ports,
+      regions: singleJumperTopology.regions,
+    }),
+  ),
+)
